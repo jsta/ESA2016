296f8e7fa6aaebb15c5bc72be8b04b27709d3f6a
jsta <jstachelek@utexas.edu>
2016-07-22 12:24:55 -0400

    add barnes chl R script

---
+++ R/spatial-variability.R
@@ -0,0 +1,5 @@
+## Create single (double?) panel to highlight chl hotspots in Barnes and saline lakes.
+
+library(DataflowR)
+
+grassmap(rnge = c(201502), mapextent = c(557217, 567415, 2786102, 2797996), params = "chlext", labelling = TRUE, label_string = "2015-02")
