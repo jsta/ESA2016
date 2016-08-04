b1c873175bca5956ee98189d43ed688b3a23a45f
jsta <jstachelek@utexas.edu>
2016-08-04 11:08:38 -0400

    reorg and resize

--- R/spatial-variability.R
+++ R/spatial-variability.R
@@ -3,3 +3,6 @@
 library(DataflowR)
 
 grassmap(rnge = c(201502), mapextent = c(557217, 567415, 2786102, 2797996), params = "chlext", labelling = TRUE, label_string = "2015-02")
+
+grassmap(rnge = 201507, params = "chlext")
+grassmap(rnge = 201502, params = "chlext")
